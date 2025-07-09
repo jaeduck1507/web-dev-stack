35111b472600ecbb55dde913905677d046b5cdbe
jaeduck1507 <67354562+jaeduck1507@users.noreply.github.com>
2025-07-09 18:23:23 +0900

    25-07-09 완의 완

--- 개인프로젝트 자바 연습/Javaproj/src/vo/PlaylistMusic.java
+++ 개인프로젝트 자바 연습/Javaproj/src/vo/PlaylistMusic.java
@@ -2,6 +2,11 @@ package vo;
 
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor @AllArgsConstructor @Data
 public class PlaylistMusic {
 	private int pmId;
 	private LocalDateTime addDate;
